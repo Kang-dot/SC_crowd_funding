7912b027618aa4567ce9ff37f3ba398a0327e866
kang-dot <jgbu97@gmail.com>
2020-01-03 16:11:28 +0900

    크라우드 펀딩 코어 부분 추가

--- blockchain_core/contracts/Crowd_funding.sol
+++ blockchain_core/contracts/Crowd_funding.sol
@@ -25,7 +25,7 @@ contract crowd_Funding {
         owner = msg.sender;
         deadline = now + _duration;
         goalAmount = _goalAmount;
-        status = "in progress...";
+        status = "in progresㅞs...";
         ended = false;
         
         numInvestors = 0;
@@ -33,38 +33,36 @@ contract crowd_Funding {
     }
     
     function fund() public payable {
-        require(!ended);
-        
+        require(!ended,"funding is over");  
+
         Investor storage inv = investors[numInvestors++];
         inv.addr = msg.sender;
         inv.amount = msg.value;
         totalAmount += inv.amount;
     }
-    
     function checkGoalReached () public onlyOwner{
-        require(!ended);
-        require(now>=deadline);
-        
+        require(!ended,"funding is over");
+        require(now>=deadline,"deadline is over");
+
         if(totalAmount >= goalAmount) {
             status = "funding success. thank you guys!";
             ended = true;
+            
             if(!owner.send(address(this).balance)){
-                revert();
+                revert("");
             }
         } else {
             uint i = 0;
             status = "funding failed...";
             ended = true;
-            
             while(i <= numInvestors) {
                 if(!investors[i].addr.send(investors[i].amount)){
-                    revert();
+                    revert("");
                 }
                 i++;
             }
         }
     }
-    
     function kill() public onlyOwner {
         selfdestruct(owner);
     }
