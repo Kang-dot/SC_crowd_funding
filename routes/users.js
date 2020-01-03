374811652346ddc62c1d88165d5674056ad6038e
kang-dot <jgbu97@gmail.com>
2020-01-03 17:29:26 +0900

    json데이터 베이스 샘플

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var funding_list = [];
+
+fs.readFile(__dirname + "/../data/" + "fundinginfo.json", "utf8", function(err, data){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(data);
+    funding_list = JSON.parse(data);
+    console.log(funding_list);
+  }
 });
 
-module.exports = router;
+router.get('/', function(req, res, next){
+  res.send('response with a resource');
+});
+
+router.get('/list', function(req, res, next){
+  res.render('fundinglist', {funding_list: funding_list});
+});
+
+router.post('/addfunding', function(req, res, next){
+  
+})
